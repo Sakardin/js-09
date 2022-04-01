260e846f3379cd105734f4d4d7348d34f33e432a
Sakardin <sakardin@tut.by>
2022-04-01 14:22:24 +0300

    1

---
+++ src/services/api.service.js
@@ -0,0 +1,50 @@
+class ApiService {
+    constructor(baseURL) {
+        this.url = baseURL
+    }
+
+    async createPost(post) {
+        try {
+            const request = new Request(this.url + '/posts.json', {
+                method: 'post',
+                body: JSON.stringify(post)
+            })
+            return useRequest(request)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    async fetchPosts() {
+        try {
+            const request = new Request(`${this.url}/posts.json`, {
+                method: 'get'
+            })
+            return useRequest(request)
+
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    async fetchPostById(id){
+        try {
+            const request = new Request(`${this.url}/posts/${id}.json`, {
+                method: 'get'
+            })
+            return useRequest(request)
+
+        } catch (error) {
+            console.error(error)
+        }
+
+    }
+}
+    async function useRequest(request) {
+        const responce = await fetch(request)
+        return await responce.json()
+
+    }
+
+
+export const apiService = new ApiService('https://js-09-seka-default-rtdb.firebaseio.com')
---
+++ src/services/transform.service.js
@@ -0,0 +1,13 @@
+export class TransformService {
+    static fbObjectToArray(fbData){
+        return Object.keys(fbData).map(key => {
+            const item = fbData[key]
+            item.id = key
+            return item
+        })
+    }
+
+    // static fbIDToTitle(fbID){
+    //
+    // }
+}
